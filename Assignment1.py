848476770bdb70e8399314058cdde8bdc1805547
baraklidsky <baraklidsky@yahoo.com>
2019-02-07 11:12:31 -0600

    Create Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,25 @@
+## Assignment1 ME369P
+## Name:
+## EID :
+##
+## Fill in the 3 functions below
+
+
+def star_wars():
+    ## Write Code Here
+    return
+
+def star_travel():
+    ## Write Code Here
+    return
+
+def calculator():
+    ## Write Code Here
+    return
+
+
+if __name__ == '__main__':
+    star_wars()
+    star_travel()
+    calculator()
+    print("Done")
